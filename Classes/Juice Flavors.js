1c061c4e11e012f481b3beccc26b83cc12a1fa0f
Robarto <robartpetrov@gmail.com>
2023-02-06 22:55:50 +0200

    100

---
+++ Classes/Juice Flavors.js
@@ -0,0 +1,62 @@
+function juiceFlavors(input) {
+  const juices = {};
+  const bottles = {};
+
+  for (const line of input) {
+    const [juice, quantity] = line.split(" => ");
+    let current = {};
+
+    if (
+      !Object.values(juices)
+        .map((x) => x.juice)
+        .includes(juice)
+    ) {
+      current = {
+        juice,
+        quantity: Number(quantity),
+      };
+      juices[juice] = current;
+    } else {
+      current = juices[juice];
+      current.quantity += Number(quantity);
+    }
+
+    let currentBottles = 0;
+
+    while (current.quantity >= 1000) {
+      currentBottles++;
+      current.quantity -= 1000;
+    }
+
+    if (
+      !Object.values(bottles)
+        .map((x) => x.juice)
+        .includes(juice) &&
+      currentBottles !== 0
+    ) {
+      current = {
+        juice,
+        bottles: currentBottles,
+      };
+      bottles[juice] = current;
+    } else if (
+      Object.values(bottles)
+        .map((x) => x.juice)
+        .includes(juice)
+    ) {
+      current = bottles[juice];
+      current.bottles += currentBottles;
+    }
+  }
+  for (const juice of Object.entries(bottles)) {
+    console.log(`${juice[0]} => ${juice[1].bottles}`);
+  }
+}
+
+juiceFlavors([
+  "Orange => 2000",
+  "Peach => 1432",
+  "Banana => 450",
+  "Peach => 600",
+  "Strawberry => 549",
+]);
